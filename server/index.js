5882434282334c65a80af8c5268658ab48f5b766
codingwalebhaiya <moneymind144@gmail.com>
2025-09-22 19:36:58 +0530

    Implement auth routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./src/routes/authRoutes";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use('/api/v1/auth', authRouter)
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
