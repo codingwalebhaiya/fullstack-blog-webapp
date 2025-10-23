e46592fc94751b35645ddb0c1910b4a12be2f1d1
codingwalebhaiya <moneymind144@gmail.com>
2025-10-23 17:24:20 +0530

    add vercel.json file

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ dbConnect();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://blog-client-gpiv.onrender.com",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
