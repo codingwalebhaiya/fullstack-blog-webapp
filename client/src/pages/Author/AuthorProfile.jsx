74cebe0ab3d0ed071a0c71dda0505e0f17cd1108
codingwalebhaiya <moneymind144@gmail.com>
2025-11-16 19:06:19 +0530

    update profile

--- client/src/pages/Author/AuthorProfile.jsx
+++ client/src/pages/Author/AuthorProfile.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import useAuth from '../../hooks/useAuth'
 
 const AuthorProfile = () => {
+  const {user} = useAuth()
   return (
-    <div>AuthorProfile</div>
+    <div>
+      <h2>Profile</h2>
+      <div>     
+      </div>
+    </div>
   )
 }
 
