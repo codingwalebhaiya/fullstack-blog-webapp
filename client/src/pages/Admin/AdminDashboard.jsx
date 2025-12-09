97e9eee5a351cf6bced661501e01aae44843aa6c
codingwalebhaiya <moneymind144@gmail.com>
2025-12-09 21:47:02 +0530

    update admin dashboard

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -3,7 +3,8 @@
 const AdminDashboard = () => (
   <div className="text-gray-800 dark:text-gray-200">
     <h2 className="text-2xl font-bold mb-4">Overview</h2>
-    <p>Welcome to the Admin Dashboard 👋</p>
+    <p>Welcome to the Admin Dashboard👋 everyone</p>
+	
   </div>
 );
 
