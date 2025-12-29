da7f7715781d3513ecda4a0101a9662c070a181f
codingwalebhaiya <moneymind144@gmail.com>
2025-12-29 20:19:43 +0530

    fix: Restore commented baseURL for local development in API utility

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,6 @@
-
-
-
 import axios from "axios";
-//  const baseURL = import.meta.env.VITE_BACKEND_URL || "https://blog-server-z30k.onrender.com";
-const baseURL = "http://backend:5000"
+const baseURL = import.meta.env.VITE_BACKEND_URL || "https://blog-server-z30k.onrender.com";
+//const baseURL = "http://backend:5000"
 const API = axios.create({
   baseURL,
   headers: {
